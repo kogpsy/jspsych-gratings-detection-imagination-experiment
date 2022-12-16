acd97451d6a638a8792af38c08a28419fdbde2a3
Andrew Ellis <a.w.ellis@gmail.com>
2022-12-16 10:43:08 +0100

    Update experiment.ts

--- src/experiment.ts
+++ src/experiment.ts
@@ -1,7 +1,7 @@
 /**
- * @title IMADET Experiment 3 Refined
+ * @title IMAPC Replication
  * @description Code for a refined replication of the third experiment of the IMADET study by Dijkstra et al. (2021).
- * @version 0.9.1
+ * @version 0.9.2
  *
  * The following lines specify which media directories will be packaged and
  * preloaded by jsPsych. Modify them to arbitrary paths (or comma-separated
